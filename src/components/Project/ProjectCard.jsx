05e7871586cf43c96a5d41ef67293c76dc24c9ca
Aulio Rovero <aulioroveroparedes@gmail.com>
2024-01-19 19:32:17 -0300

    Se agrego CSS para mantener las cards de los proyectos en el centro

--- src/components/Project/ProjectCard.jsx
+++ src/components/Project/ProjectCard.jsx
@@ -3,13 +3,13 @@ import { dataProjects } from "../../shared/enums";
 
 const ProjectCard = () => {
    return (
-      <div className="row">
+      <div className="row ">
          {dataProjects.map((project) => (
-            <div className="col-md-3" key={project.id} >
+            <div className="col-md-3 centered-card" key={project.id} >
             <div className="card mb-2 " style={{width: "18rem"}} >
       <img src={project.image} className="card-img-top" alt={project.title} />
       <div className="card-body">
-        <h5 className="card-title">{project.title}</h5>
+        <h5 className="card-title">{project.title}</h5> 
         <p className="card-text">{project.description}</p>
       </div>
       <h6>Tecnologias aplicadas:</h6>
