73df2f76277c6e38115e5a22ad9a69143ec911cf
Aulio Rovero <aulioroveroparedes@gmail.com>
2024-01-31 14:48:32 -0300

    se trabajo en el componente ProjectCard se alinearon las cards

--- src/components/Project/ProjectCard.jsx
+++ src/components/Project/ProjectCard.jsx
@@ -1,49 +1,52 @@
-import "./project.css"
+import "./project.css";
 import { dataProjects } from "../../shared/enums";
 
 const ProjectCard = () => {
    return (
-      <div className="row ">
+      <div className="row d-flex align-items-stretch justify-content-center">
          {dataProjects.map((project) => (
-            <div className="col-md-3 centered-card" key={project.id} >
-            <div className="card mb-2 " style={{width: "18rem"}} >
-      <img src={project.image} className="card-img-top" alt={project.title} />
-      <div className="card-body">
-        <h5 className="card-title">{project.title}</h5> 
-        <p className="card-text">{project.description}</p>
-      </div>
-      <h6>Tecnologias aplicadas:</h6>
-      <div className="card-body d-flex justify-content-center " style={{ maxWidth: "100%", overflow: "hidden" }}>
-         <div  className="icon-row">{Object.keys(project.technologies).map((technology, index) => (
-                        <span key={index} className="me-2">
-                           {project.technologies[technology]({ size: "2em" })}
-                        </span>
-                     ))}</div>
-                     
+            <div className="col-md-3 centered-card" key={project.id}>
+              <div className="project-card card mb-3" style={{ width: "18rem" }}>
+                <img src={project.image} className="card-img-top" alt={project.title} />
+                <div className="card-body d-flex flex-column align-items-center">
+                  <h5 className="card-title">{project.title}</h5>
+                  <p className="card-text">{project.description}</p>
+                  <h6>Tecnologias aplicadas:</h6>
+                  <div className="icon-row d-flex justify-content-center">
+                    {Object.keys(project.technologies).map((technology, index) => (
+                      <span key={index} className="me-2">
+                        {project.technologies[technology]({ size: "2em" })}
+                      </span>
+                    ))}
                   </div>
-      <div className="card-body">
-      {project.urlDeploy.length > 0 ? (
-                <a
-                  href={project.urlDeploy}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="card-link"
-                >
-                  Ver Proyecto
-                </a>
-              ) : (
-                <p className="text-danger">URL no disponible</p>
-              )}
-        <a href={project.urlRepo}
-        target="_blank" 
-        rel="noopener noreferrer"
-        className="card-link">Ver Repositorio</a>
-      </div>
-    </div>
-    </div>
+                  <div className="d-flex flex-column align-items-center">
+                    {project.urlDeploy.length > 0 ? (
+                      <a
+                        href={project.urlDeploy}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="card-link"
+                      >
+                        Ver Proyecto
+                      </a>
+                    ) : (
+                      <p className="text-danger">URL no disponible</p>
+                    )}
+                    <a
+                      href={project.urlRepo}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="card-link"
+                    >
+                      Ver Repositorio
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
          ))}
-      
-    </div>
+      </div>
    );
 };
+
 export default ProjectCard;
