22e8aac63639cb4bbf5340d5a0b082672eb60fbe
Aulio Rovero <aulioroveroparedes@gmail.com>
2024-02-07 19:55:59 -0300

    Se agrego margen al componente AboutMe y padding en el componente Work

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -2,6 +2,7 @@ import AboutMeCard from "./AboutMeCard";
 const AboutMeComponent = () => {
    return (
       <div id="aboutMe" className="container col-md-10 col-lg-10 col-xl-9">
+         <h1 className="text-dark mb-2 ">Un poco sobre mi</h1>
          <AboutMeCard />
       </div>
    );
--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -2,7 +2,7 @@ import WorkCard from "./WorkCard";
 
    const WorkComponent = () => {
       return (
-         <div id="work" className="container">
+         <div id="work" className="container px-4">
             <h1 className="text-dark mb-5 ">Mis experiencias laborales</h1>
          <WorkCard/>
          </div>
